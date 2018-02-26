38f9b80298b78267969e53e8745b0a12fa9dbb17
advancingKoala <milaiduoduo4567@163.com>
2018-02-26 18:31:03 +0800

    feat:使用中间件koa-views来解析ejs模板页面，并新增一个EJS模板页面

--- server/index.js
+++ server/index.js
@@ -1,10 +1,20 @@
 const Koa = require('koa');
 const app = new Koa();
+const views = require('koa-views');
+const {resolve} = require('path');
+console.log('dirname:%s 是不是？',__dirname);
+
+// resolve(__dirname,'./views')，就是取得绝对路径
+//extension是设置要解析的模板后缀
+app.use(views(resolve(__dirname,'./views'),{
+    extension:'ejs'
+}));
+
 app.use(async(ctx,next)=>{
-    ctx.body = '电影首页(use async)'
-});
-// app.use(function*(next){
-//     this.body = '电影首页 use generator!!!';
-// })
-app.listen(4455);
-console.log('koacrawler runing in port 4455..............')
+    await ctx.render('index',{
+        name:'alice',
+        food:'过桥米线！！'
+    })
+})
+app.listen(4000);
+console.log('koacrawler runing in port 4000..............')
